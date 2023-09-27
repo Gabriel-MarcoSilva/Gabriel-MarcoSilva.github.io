3a6206683c6d8e9c7d7e2b60abe8cede985d3517
Gabriel <gabrielmarkolinosilva14@gmail.com>
2023-09-27 10:09:34 -0300

    feat: add metodo POST e favicon

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 getBtn = document.getElementById("getBtn")
 containerCards = document.getElementById("container-cards")
 
+baseURL = "https://reqres.in/api"
+
 arrayData = []
 
-const requestHTTP = (method, url) =>{
+const requestHTTP = (method, url, data) => {
 
-    const promise = new Promise((resolve, reject) =>{
-        const xhr = new  XMLHttpRequest()
-        
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+
         xhr.open(method, url)
-    
-        xhr.responseType = 'json'
-        
-        xhr.onload = () =>{
-    
-            resolve(xhr.response)
-            // arrayData = []
-            // const data = JSON.parse(xhr.response)
-            // arrayData = data.data
-        
-            // arrayData.map((item) =>{
-            //     containerCards.appendChild(document.createElement("p").appendChild(document.createTextNode(item.first_name)))
-            //     containerCards.appendChild(document.createElement("br"))
-            // })
-        
+
+        //xhr.responseType = 'json'
+
+        data ? xhr.setRequestHeader('Content-Type', 'application/json') : null
+
+        xhr.onload = () => {
+
+            if (xhr.status >= 400) {
+                reject(chr.response)
+            } else {
+                resolve(xhr.response)
+
+                arrayData = []
+
+                if (!data) {
+                    const info = JSON.parse(xhr.response)
+                    arrayData = info.data
+
+                    arrayData.map((item) => {
+                        containerCards.appendChild(document.createElement("p").appendChild(document.createTextNode(item.first_name)))
+                        containerCards.appendChild(document.createElement("br"))
+                    })
+                }
+            }
+
         }
-        
-        xhr.send()
+
+        xhr.onerror = () => {
+            reject("erro de comunicação")
+        }
+
+        xhr.send(JSON.stringify(data))
     })
 
     return promise
 }
 
-const getData = () =>{
-    requestHTTP("GET", "https://reqres.in/api/users").then(responseData =>{
-        console.log(responseData)
+function getData() {
+    requestHTTP("GET", `https://reqres.in/api/users`).then(responseData => {
+        //console.log(responseData)
     })
 }
 
-getBtn.addEventListener('click', getData)
+function postData() {
+    requestHTTP("POST", `${baseURL}/register`, {
+        email: "eve.holt@reqres.in",
+        password: "pistol"
+    }).then(responseData => {
+        console.log(responseData)
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+getBtn.addEventListener('click', postData)
