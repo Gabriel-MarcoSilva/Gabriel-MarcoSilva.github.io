e60f99e5c69599396a0c83adac33dbb45cb4da1f
Gabriel <gabrielmarkolinosilva14@gmail.com>
2023-09-27 11:16:11 -0300

    chore: testando a implementação com gifs

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 getBtn = document.getElementById("getBtn")
 containerCards = document.getElementById("container-cards")
 
+
 baseURL = "https://reqres.in/api"
 
 arrayData = []
 
 const requestHTTP = (method, url, data) => {
-
+    
     const promise = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
-
+        
         xhr.open(method, url)
 
         //xhr.responseType = 'json'
-
+        
         data ? xhr.setRequestHeader('Content-Type', 'application/json') : null
-
+        
         xhr.onload = () => {
-
+            
             if (xhr.status >= 400) {
                 reject(chr.response)
             } else {
                 resolve(xhr.response)
-
+                
                 arrayData = []
-
+                
                 if (!data) {
                     const info = JSON.parse(xhr.response)
                     arrayData = info.data
 
+                    console.log(arrayData)
+                    
                     arrayData.map((item) => {
-                        containerCards.appendChild(document.createElement("p").appendChild(document.createTextNode(item.first_name)))
-                        containerCards.appendChild(document.createElement("br"))
+                        let card = document.createElement("div")
+                        let img = document.createElement("div")
+                        img.style.backgroundImage = "url(images/gif.gif)"
+                        img.className = "img-card"
+                        card.className = "card"
+                        card.appendChild(document.createElement("p").appendChild(document.createTextNode("Nome: " + item.first_name)))
+                        card.appendChild(img)
+                        card.appendChild(document.createElement("p").appendChild(document.createTextNode(item.email)))
+                        containerCards.appendChild(card)
                     })
                 }
             }
